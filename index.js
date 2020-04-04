5e0d06857e49097d028ad15acbae8e7547a07c1a
vicety <cyhuang.aya@outlook.com>
2020-04-04 20:54:27 +0800

    fix url

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post('/upload', upload.any(), async (req, res, next) => {
 app.post('/most_similar', bodyParser.json(), async function (req, res, next) {
   // logger.debug(req.body.pattern[0])
   
-  axios.post('http://localhost:21112/predict_order', {
+  axios.post('http://localhost:21112/most_similar', {
     "word": req.body.word,
     "pattern": req.body.pattern
   }).then(result => {
